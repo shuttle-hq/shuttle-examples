2f61afdda7a36ce810fd67f50a51b03f12be37bc
max143672 <biryukov.maxim.vladimirovich@yandex.ru>
2022-11-27 11:12:33 +0400

    add actix-web example

---
+++ actix-web/hello-world/src/lib.rs
@@ -0,0 +1,14 @@
+use actix_web::web::{resource, ServiceConfig};
+use shuttle_service::{ShuttleActixWeb};
+
+async fn hello_world() -> &'static str {
+    "Hello World!"
+}
+
+#[shuttle_service::main]
+async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Copy + Clone + 'static> {
+    let h = hello_world;
+    Ok(move |cfg: &mut ServiceConfig| {
+        cfg.service(resource("/").to(h));
+    })
+}
