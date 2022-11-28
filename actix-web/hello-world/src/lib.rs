625470f86286e899a8a0a518e28db49e956f0244
max143672 <biryukov.maxim.vladimirovich@yandex.ru>
2022-11-28 22:27:43 +0400

    fmt + sort

--- actix-web/hello-world/src/lib.rs
+++ actix-web/hello-world/src/lib.rs
@@ -1,12 +1,13 @@
 use actix_web::web::{resource, ServiceConfig};
-use shuttle_service::{ShuttleActixWeb};
+use shuttle_service::ShuttleActixWeb;
 
 async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
 #[shuttle_service::main]
-async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Copy + Clone + 'static> {
+async fn actix_web(
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Copy + Clone + 'static> {
     let h = hello_world;
     Ok(move |cfg: &mut ServiceConfig| {
         cfg.service(resource("/").to(h));
