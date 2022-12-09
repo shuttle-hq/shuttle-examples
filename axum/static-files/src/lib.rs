2a05b9a5e7a8a70b04b66daeee1601674bda9c15
Oddbjørn Grødem <29732646+oddgrd@users.noreply.github.com>
2022-12-09 16:52:34 +0100

    feat: add axum static files example (#6)

---
+++ axum/static-files/src/lib.rs
@@ -0,0 +1,24 @@
+use std::path::PathBuf;
+
+use axum::{routing::get, Router};
+use axum_extra::routing::SpaRouter;
+use sync_wrapper::SyncWrapper;
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[shuttle_service::main]
+async fn axum(
+    // Name your static assets folder by passing `folder = <name>` to `StaticFolder`
+    // If you don't pass a name, it will default to `static`.
+    #[shuttle_static_folder::StaticFolder(folder = "assets")] static_folder: PathBuf,
+) -> shuttle_service::ShuttleAxum {
+    let router = Router::new()
+        .route("/hello", get(hello_world))
+        .merge(SpaRouter::new("/assets", static_folder).index_file("index.html"));
+
+    let sync_wrapper = SyncWrapper::new(router);
+
+    Ok(sync_wrapper)
+}
