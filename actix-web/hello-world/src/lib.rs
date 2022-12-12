893bc39babf64aedeafa5dbba5d11806b163d57a
Oddbjørn Grødem <29732646+oddgrd@users.noreply.github.com>
2022-12-12 11:14:33 +0100

    feat: add actix postgres example (#8)

--- actix-web/hello-world/src/lib.rs
+++ actix-web/hello-world/src/lib.rs
@@ -1,14 +1,15 @@
-use actix_web::web::{resource, ServiceConfig};
+use actix_web::{get, web::ServiceConfig};
 use shuttle_service::ShuttleActixWeb;
 
+#[get("/hello")]
 async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
 #[shuttle_service::main]
 async fn actix_web(
-) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Copy + Clone + 'static> {
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Clone + 'static> {
     Ok(move |cfg: &mut ServiceConfig| {
-        cfg.service(resource("/hello").to(hello_world));
+        cfg.service(hello_world);
     })
 }
