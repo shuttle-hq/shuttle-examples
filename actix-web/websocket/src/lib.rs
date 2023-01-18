e8350e54519907c46d713d0dd89d31fb73ff7a2e
Roberto Huertas <roberto.huertas@outlook.com>
2023-01-18 20:18:19 +0100

    simple version working

--- actix-web/websocket/src/lib.rs
+++ actix-web/websocket/src/lib.rs
@@ -8,47 +8,56 @@ use actix_web_actors::ws;
 use chrono::{DateTime, Utc};
 use serde::Serialize;
 use shuttle_service::ShuttleActixWeb;
-use std::{sync::Arc, time::Duration};
-use tokio::sync::{watch, Mutex};
+use std::{
+    sync::{atomic::AtomicUsize, Arc},
+    time::Duration,
+};
+use tokio::sync::{mpsc, watch};
 
 const PAUSE_SECS: u64 = 15;
 const STATUS_URI: &str = "https://api.shuttle.rs";
 
-struct AppState {
-    clients_count: usize,
-    rx: watch::Receiver<Response>,
+type AppState = (
+    mpsc::UnboundedSender<WsState>,
+    watch::Receiver<ApiStateMessage>,
+);
+
+#[derive(Debug)]
+enum WsState {
+    Connected,
+    Disconnected,
 }
 
 #[derive(Serialize, actix::Message, Default, Clone, Debug)]
 #[rtype(result = "()")]
-struct Response {
+struct ApiStateMessage {
     clients_count: usize,
     date_time: DateTime<Utc>,
     is_up: bool,
 }
 
 struct WsActor {
-    app_state: Arc<Mutex<AppState>>,
+    app_state: Arc<AppState>,
 }
 
 impl WsActor {
-    fn new(app_state: Arc<Mutex<AppState>>) -> Self {
+    fn new(app_state: Arc<AppState>) -> Self {
         Self { app_state }
     }
 
     fn start(&mut self, ctx: &mut ws::WebsocketContext<Self>) {
         let recipient = ctx.address().recipient();
-        let state = self.app_state.clone();
+        let tx_ws_state = self.app_state.0.clone();
+        let mut rx_api_state = self.app_state.1.clone();
+
+        if let Err(e) = tx_ws_state.send(WsState::Connected) {
+            tracing::error!("Failed to send connected state: {e:?}");
+        }
 
         let fut = async move {
-            let mut rx = {
-                let mut state = state.lock().await;
-                state.clients_count += 1;
-                state.rx.clone()
-            };
-
-            while let Ok(()) = rx.changed().await {
-                let msg = rx.borrow().clone();
+            // listen to the state channel
+            while let Ok(()) = rx_api_state.changed().await {
+                let msg = rx_api_state.borrow().clone();
                 tracing::info!("WS: Sending {msg:?}");
                 recipient.do_send(msg);
             }
@@ -66,10 +75,10 @@ impl Actor for WsActor {
     }
 }
 
-impl Handler<Response> for WsActor {
+impl Handler<ApiStateMessage> for WsActor {
     type Result = ();
 
-    fn handle(&mut self, msg: Response, ctx: &mut Self::Context) {
+    fn handle(&mut self, msg: ApiStateMessage, ctx: &mut Self::Context) {
         let msg = serde_json::to_string(&msg).unwrap();
         ctx.text(msg);
     }
@@ -81,11 +90,10 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsActor {
         match msg {
             Ok(ws::Message::Text(text)) => ctx.text(text),
             Ok(ws::Message::Close(reason)) => {
-                let state = self.app_state.clone();
-                tokio::spawn(async move {
-                    let mut state = state.lock().await;
-                    state.clients_count -= 1;
-                });
+                let tx = self.app_state.0.clone();
+                if let Err(e) = tx.send(WsState::Disconnected) {
+                    tracing::error!("Failed to send connected state: {e:?}");
+                }
                 ctx.close(reason)
             }
             _ => (),
@@ -96,7 +104,7 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsActor {
 async fn websocket(
     req: HttpRequest,
     stream: web::Payload,
-    app_state: web::Data<Mutex<AppState>>,
+    app_state: web::Data<AppState>,
 ) -> actix_web::Result<HttpResponse> {
     let response = ws::start(WsActor::new(app_state.into_inner().clone()), &req, stream);
     tracing::info!("New WS: {response:?}");
@@ -112,31 +120,33 @@ async fn index() -> impl Responder {
 #[shuttle_service::main]
 async fn actix_web(
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Sync + Send + Clone + 'static> {
-    let (tx, rx) = watch::channel(Response::default());
+    // As we cannot use the `actix_web::main` macro a pure actor based implementation
+    // In order for a System to be created we need to have access to the instance of the Tokio runtime.
+    // For the moment, we're going to use channels to communicate between threads.
+    // api state channel
+    let (tx_api_state, rx_api_state) = watch::channel(ApiStateMessage::default());
+    // websocket state channel
+    let (tx_ws_state, mut rx_ws_state) = mpsc::unbounded_channel::<WsState>();
 
-    let state = Arc::new(Mutex::new(AppState {
-        clients_count: 0,
-        rx,
-    }));
+    let client_count = Arc::new(AtomicUsize::new(0));
 
     // Spawn a thread to continually check the status of the api
-    let state_send = state.clone();
-
+    let client_count_send = client_count.clone();
+    let client = reqwest::Client::default();
+    let client = client.clone();
     tokio::spawn(async move {
         let duration = Duration::from_secs(PAUSE_SECS);
-        let client = reqwest::Client::default();
 
         loop {
-            let is_up = client.get(STATUS_URI).send().await;
-            let is_up = is_up.is_ok();
+            let is_up = get_api_status(&client).await;
 
-            let response = Response {
-                clients_count: state_send.lock().await.clients_count,
+            let response = ApiStateMessage {
+                clients_count: client_count_send.load(std::sync::atomic::Ordering::SeqCst),
                 date_time: Utc::now(),
                 is_up,
             };
 
-            if tx.send(response).is_err() {
+            if tx_api_state.send(response).is_err() {
                 break;
             }
 
@@ -144,7 +154,21 @@ async fn actix_web(
         }
     });
 
-    let app_state = web::Data::from(state);
+    // spawn a thread to continually check the status of the websocket connections
+    tokio::spawn(async move {
+        while let Some(state) = rx_ws_state.recv().await {
+            match state {
+                WsState::Connected => {
+                    client_count.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                }
+                WsState::Disconnected => {
+                    client_count.fetch_sub(1, std::sync::atomic::Ordering::SeqCst);
+                }
+            }
+        }
+    });
+
+    let app_state = web::Data::new((tx_ws_state, rx_api_state));
 
     Ok(move |cfg: &mut ServiceConfig| {
         cfg.service(web::resource("/").route(web::get().to(index)))
@@ -154,4 +178,9 @@ async fn actix_web(
                     .route(web::get().to(websocket)),
             );
     })
+}
+
+async fn get_api_status(client: &reqwest::Client) -> bool {
+    let response = client.get(STATUS_URI).send().await;
+    response.is_ok()
 }
