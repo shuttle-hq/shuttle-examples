5f9a10938d6d19e12deeb462497403de27fe966c
Pieter <pieter@chesedo.me>
2023-02-06 12:40:02 +0200

    feat: next server example (#16)
    * feat: next server example
    
    * refactor: update readme
    
    * refactor: readme title
    
    * fix: remove old ticks
    
    ---------
    
    Co-authored-by: Oddbjørn Grødem <29732646+oddgrd@users.noreply.github.com>

---
+++ axum/static-next-server/README.md
@@ -0,0 +1,3 @@
+# Serving a NextJS project with Axum
+
+This example is a template for an [axum](https://github.com/tokio-rs/axum) server that serves a NextJS single-page application as static assets, deployed with shuttle. For more information on how to achieve this, check out this article by one of our contributors:  https://joshmo.hashnode.dev/deploying-a-nextjs-front-end-with-a-rust-api-in-one-go
---
+++ axum/static-next-server/src/lib.rs
@@ -0,0 +1,17 @@
+use std::path::PathBuf;
+
+use axum::Router;
+use axum_extra::routing::SpaRouter;
+use sync_wrapper::SyncWrapper;
+
+#[shuttle_service::main]
+async fn axum(
+    #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
+) -> shuttle_service::ShuttleAxum {
+    let router =
+        Router::new().merge(SpaRouter::new("/assets", static_folder).index_file("index.html"));
+
+    let sync_wrapper = SyncWrapper::new(router);
+
+    Ok(sync_wrapper)
+}
