0642385826a0c5aa58ff5e9647c6c912534d797b
jhawkesworth <jhawkesworth@users.noreply.github.com>
2023-04-20 12:23:39 +0100

    feat examples: add rocket dynamic template (shuttle-static-folder) example (#38)
    * feat examples: add rocket static files and dynamic template example projects.
    
    * feat examples: remove unneeded static-files rocket example
    
    * examples docs: fix incorrect comment
    
    * feat examples: code now formatted with rustfmt and comment regarding serving static content

---
+++ rocket/dyn_template_hbs/src/main.rs
@@ -0,0 +1,46 @@
+#[macro_use]
+extern crate rocket;
+// uncomment if you also wish to serve static resources
+// use rocket::fs::{FileServer, relative};
+use rocket::response::Redirect;
+use rocket_dyn_templates::{context, Template};
+use std::path::PathBuf;
+
+#[get("/")]
+fn index() -> Redirect {
+    Redirect::to(uri!("/", hello(name = "Your Name")))
+}
+#[get("/hello/<name>")]
+pub fn hello(name: &str) -> Template {
+    Template::render(
+        "index",
+        context! {
+            title: "Hello",
+            name: Some(name),
+            items: vec!["Example", "List", "Of", "Five", "Items"],
+        },
+    )
+}
+
+#[shuttle_runtime::main]
+async fn rocket(
+    #[shuttle_static_folder::StaticFolder(folder = "templates")] static_folder: PathBuf,
+) -> shuttle_rocket::ShuttleRocket {
+    /* The provisioned static folder template directory will not be a sub folder
+       of the location of the executable so it is necessary to merge the
+       template_dir setting into the configuration at runtime so that dynamic templates work.
+
+       Note that shuttle does not include Rocket.toml
+       so merging config is the preferred way to modify any settings
+       that would otherwise be set in Rocket.toml
+    */
+    let template_dir = static_folder.to_str().unwrap();
+    let figment = rocket::Config::figment().merge(("template_dir", template_dir));
+    let rocket = rocket::custom(figment)
+        // If you also wish to serve static content, uncomment line below and corresponding 'use' on line 4
+        // .mount("/", FileServer::from(relative!("templates")))
+        .mount("/", routes![index, hello])
+        .attach(Template::fairing());
+
+    Ok(rocket.into())
+}
