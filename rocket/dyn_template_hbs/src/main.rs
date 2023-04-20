051f53211b44b15ff6506fb08b39f9d8014d1462
Jon Hawkesworth <jhawkesworth@users.noreply.github.com>
2023-04-20 06:21:55 +0100

    feat examples: code now formatted with rustfmt and comment regarding serving static content

--- rocket/dyn_template_hbs/src/main.rs
+++ rocket/dyn_template_hbs/src/main.rs
@@ -1,9 +1,10 @@
 #[macro_use]
 extern crate rocket;
+// uncomment if you also wish to serve static resources
 // use rocket::fs::{FileServer, relative};
-use std::path::PathBuf;
 use rocket::response::Redirect;
-use rocket_dyn_templates::{Template, context};
+use rocket_dyn_templates::{context, Template};
+use std::path::PathBuf;
 
 #[get("/")]
 fn index() -> Redirect {
@@ -11,32 +12,35 @@ fn index() -> Redirect {
 }
 #[get("/hello/<name>")]
 pub fn hello(name: &str) -> Template {
-    Template::render("index", context! {
-        title: "Hello",
-        name: Some(name),
-        items: vec!["Example", "List", "Of", "Five", "Items"],
-    })
+    Template::render(
+        "index",
+        context! {
+            title: "Hello",
+            name: Some(name),
+            items: vec!["Example", "List", "Of", "Five", "Items"],
+        },
+    )
 }
 
 #[shuttle_runtime::main]
-async fn rocket(#[shuttle_static_folder::StaticFolder(folder="templates")] static_folder: PathBuf) -> shuttle_rocket::ShuttleRocket {
-
-    /* The provisioned static folder template directory will not be a sub folder 
-       of the location of the executable so it is necessary to merge the 
+async fn rocket(
+    #[shuttle_static_folder::StaticFolder(folder = "templates")] static_folder: PathBuf,
+) -> shuttle_rocket::ShuttleRocket {
+    /* The provisioned static folder template directory will not be a sub folder
+       of the location of the executable so it is necessary to merge the
        template_dir setting into the configuration at runtime so that dynamic templates work.
 
-       Note that shuttle does not include Rocket.toml 
+       Note that shuttle does not include Rocket.toml
        so merging config is the preferred way to modify any settings
        that would otherwise be set in Rocket.toml
     */
     let template_dir = static_folder.to_str().unwrap();
-    let figment = rocket::Config::figment()
-	.merge(("template_dir", template_dir));
+    let figment = rocket::Config::figment().merge(("template_dir", template_dir));
     let rocket = rocket::custom(figment)
-	// .mount("/", FileServer::from(relative!("templates")))
-	.mount("/", routes![index, hello])
-        .attach(Template::fairing())
-        ;
+        // If you also wish to serve static content, uncomment line below and corresponding 'use' on line 4
+        // .mount("/", FileServer::from(relative!("templates")))
+        .mount("/", routes![index, hello])
+        .attach(Template::fairing());
 
     Ok(rocket.into())
 }
