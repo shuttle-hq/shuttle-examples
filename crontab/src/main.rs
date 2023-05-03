412febbd95251ba73488b7d936a3a3b9e395462c
Krzysztof Urbaniak <urban@fail.pl>
2023-05-03 18:35:50 +0200

    run CronService job in a loop

--- crontab/src/main.rs
+++ crontab/src/main.rs
@@ -1,5 +1,6 @@
 use chrono::Utc;
 use std::{future::Future, str::FromStr};
+use tokio::time::sleep;
 
 use cron::Schedule;
 use shuttle_runtime::async_trait;
@@ -11,15 +12,13 @@ pub struct CronService<F> {
 
 impl<F: Future> CronService<F> {
     async fn start(&self) {
-        for time in self.schedule.upcoming(Utc).take(4) {
-            let now = chrono::offset::Utc::now();
-            let dur = time.signed_duration_since(now);
-
-            let now_tok = tokio::time::Instant::now();
-            if let Some(instant) = now_tok.checked_add(dur.to_std().unwrap()) {
-                tokio::time::sleep_until(instant).await;
-                (self.job)().await;
-            }
+        while let Some(next_run) = self.schedule.upcoming(Utc).next() {
+            let next_run_in = next_run
+                .signed_duration_since(chrono::offset::Utc::now())
+                .to_std()
+                .unwrap();
+            sleep(next_run_in).await;
+            (self.job)().await;
         }
     }
 }
@@ -49,7 +48,7 @@ async fn my_job() {
 #[shuttle_runtime::main]
 async fn init(
 ) -> Result<CronService<impl Future<Output = ()> + Send + 'static>, shuttle_service::Error> {
-    let schedule = Schedule::from_str("*/1 * * * * *").unwrap();
+    let schedule = Schedule::from_str("*/5 * * * * *").unwrap();
     Ok(CronService {
         schedule,
         job: my_job,
