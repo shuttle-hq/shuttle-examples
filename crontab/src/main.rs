d2dab07ecb5542b0a7ea0550ffd2f15b25be3300
morlinbrot <morlinbrot@mailbox.org>
2023-05-03 12:32:45 +0200

    Add simple job execution

--- crontab/src/main.rs
+++ crontab/src/main.rs
@@ -1,33 +1,57 @@
-use axum::Router;
+use chrono::Utc;
+use std::{future::Future, str::FromStr};
+
+use cron::Schedule;
 use shuttle_runtime::async_trait;
 
-mod router;
+pub struct CronService<F> {
+    schedule: Schedule,
+    job: fn() -> F,
+}
 
-pub struct CronService {
-    router: Router,
+impl<F: Future> CronService<F> {
+    async fn start(&self) {
+        for time in self.schedule.upcoming(Utc).take(4) {
+            let now = chrono::offset::Utc::now();
+            let dur = time.signed_duration_since(now);
+
+            let now_tok = tokio::time::Instant::now();
+            if let Some(instant) = now_tok.checked_add(dur.to_std().unwrap()) {
+                tokio::time::sleep_until(instant).await;
+                (self.job)().await;
+            }
+        }
+    }
 }
 
 #[async_trait]
-impl shuttle_service::Service for CronService {
+impl<F> shuttle_service::Service for CronService<F>
+where
+    F: Future + Send + Sync + 'static,
+{
     async fn bind(
         mut self,
-        addr: std::net::SocketAddr,
+        _addr: std::net::SocketAddr,
     ) -> Result<(), shuttle_service::error::Error> {
-        let router = self.router;
+        self.start().await;
 
-        let serve_router = axum::Server::bind(&addr).serve(router.into_make_service());
-
-        tokio::select!(
-            _ = serve_router => {}
-        );
+        println!("All done.");
 
         Ok(())
     }
 }
 
-#[shuttle_runtime::main]
-async fn init() -> Result<CronService, shuttle_service::Error> {
-    let router = router::build_router();
+async fn my_job() {
+    let now = chrono::offset::Utc::now();
+    println!("It is {}", now.format("%Y-%m-%d %H:%M:%S"));
+}
 
-    Ok(CronService { router })
+#[shuttle_runtime::main]
+async fn init(
+) -> Result<CronService<impl Future<Output = ()> + Send + 'static>, shuttle_service::Error> {
+    let schedule = Schedule::from_str("*/1 * * * * *").unwrap();
+    Ok(CronService {
+        schedule,
+        job: my_job,
+    })
 }
