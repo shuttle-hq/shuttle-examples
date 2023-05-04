82c73d7d693a609ef26222b1526bbbd1545b98e5
morlinbrot <morlinbrot@mailbox.org>
2023-05-04 11:11:50 +0200

    Add some documentation

--- crontab/src/main.rs
+++ crontab/src/main.rs
@@ -5,6 +5,15 @@ use tokio::time::sleep;
 use cron::Schedule;
 use shuttle_runtime::async_trait;
 
+// "Run every 2 seconds"
+const SCHEDULE: &str = "*/2 * * * * *";
+
+// The function that will be run.
+async fn my_job() {
+    let now = chrono::offset::Utc::now();
+    println!("It is {}", now.format("%Y-%m-%d %H:%M:%S"));
+}
+
 pub struct CronService<F> {
     schedule: Schedule,
     job: fn() -> F,
@@ -40,15 +49,10 @@ where
     }
 }
 
-async fn my_job() {
-    let now = chrono::offset::Utc::now();
-    println!("It is {}", now.format("%Y-%m-%d %H:%M:%S"));
-}
-
 #[shuttle_runtime::main]
 async fn init(
 ) -> Result<CronService<impl Future<Output = ()> + Send + 'static>, shuttle_service::Error> {
-    let schedule = Schedule::from_str("*/5 * * * * *").unwrap();
+    let schedule = Schedule::from_str(SCHEDULE).unwrap();
     Ok(CronService {
         schedule,
         job: my_job,
