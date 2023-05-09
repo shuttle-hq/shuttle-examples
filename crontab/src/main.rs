33d408c0d704b26543ff6b716dd267d3550c8103
morlinbrot <morlinbrot@mailbox.org>
2023-05-09 17:55:08 +0200

    Move CronService to its own crate

--- crontab/src/main.rs
+++ crontab/src/main.rs
@@ -1,10 +1,8 @@
-use chrono::Utc;
 use std::{future::Future, str::FromStr};
-use tokio::time::sleep;
-use tracing::info;
 
 use cron::Schedule;
-use shuttle_runtime::{async_trait, Service};
+use shuttle_crontab::CronService;
+use shuttle_runtime::tracing::info;
 
 // "Run every 2 seconds"
 const SCHEDULE: &str = "*/2 * * * * *";
@@ -13,39 +11,6 @@ const SCHEDULE: &str = "*/2 * * * * *";
 async fn my_job() {
     let now = chrono::offset::Utc::now();
     info!("It is {}", now.format("%Y-%m-%d %H:%M:%S"));
-}
-
-pub struct CronService<F> {
-    schedule: Schedule,
-    job: fn() -> F,
-}
-
-impl<F: Future> CronService<F> {
-    async fn start(&self) {
-        while let Some(next_run) = self.schedule.upcoming(Utc).next() {
-            let next_run_in = next_run
-                .signed_duration_since(chrono::offset::Utc::now())
-                .to_std()
-                .unwrap();
-            sleep(next_run_in).await;
-            (self.job)().await;
-        }
-    }
-}
-
-#[async_trait]
-impl<F> Service for CronService<F>
-where
-    F: Future + Send + Sync + 'static,
-{
-    async fn bind(
-        mut self,
-        _addr: std::net::SocketAddr,
-    ) -> Result<(), shuttle_service::error::Error> {
-        self.start().await;
-
-        Ok(())
-    }
 }
 
 #[shuttle_runtime::main]
