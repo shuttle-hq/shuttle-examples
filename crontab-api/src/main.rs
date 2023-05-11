1633d462c70e544bde0a43b8cfb2d5d22dc96fc9
morlinbrot <morlinbrot@mailbox.org>
2023-05-11 11:05:54 +0200

    Make use of CrontabService::new now accepting a router

--- crontab-api/src/main.rs
+++ crontab-api/src/main.rs
@@ -1,7 +1,13 @@
+use axum::{response::IntoResponse, routing::get, Router};
 use shuttle_crontab::{CrontabService, ShuttleCrontab};
 use shuttle_persist::{Persist, PersistInstance};
 
+async fn hello_crontab() -> impl IntoResponse {
+    "Hello there, try making a POST request to '/crontab/set' to create a new job.".to_string()
+}
+
 #[shuttle_runtime::main]
 async fn crontab(#[Persist] persist: PersistInstance) -> ShuttleCrontab {
-    CrontabService::new(persist)
+    let router = Router::new().route("/", get(hello_crontab));
+    CrontabService::new(persist, router)
 }
