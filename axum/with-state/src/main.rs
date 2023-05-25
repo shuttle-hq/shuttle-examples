89fe077ac7546e1551e01f855f41e6cb8f0ddabd
morlinbrot <morlinbrot@mailbox.org>
2023-05-25 14:42:53 +0200

    chore: Remove local dependencies

--- axum/with-state/src/main.rs
+++ axum/with-state/src/main.rs
@@ -3,18 +3,18 @@ use std::sync::Arc;
 use axum::{extract::State, response::IntoResponse, routing::get, Router};
 
 async fn hello_world(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    state.name
+    state.msg
 }
 
 #[derive(Clone)]
 struct AppState {
-    name: &'static str,
+    msg: &'static str,
 }
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
     let state = Arc::new(AppState {
-        name: "Hello, world!",
+        msg: "Hello, world!",
     });
 
     let router = Router::new().route("/", get(hello_world)).with_state(state);
