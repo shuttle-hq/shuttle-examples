f56eae976d6ddf22a7e19159b064239515790830
Paul Otten <lightnica@yahoo.ca>
2023-06-20 20:27:46 +0100

    update qdrant example - `shuttle_qdrant::Qdrant` now returns a `QdrantClient` - pass in `cloud_url` and `api_key`, read from `Secrets.toml`

--- axum/qdrant/src/main.rs
+++ axum/qdrant/src/main.rs
@@ -12,18 +12,14 @@ async fn list_collections(State(state): State<Arc<AppState>>) -> String {
 
 #[shuttle_runtime::main]
 async fn axum(
-    #[shuttle_qdrant::Qdrant] info: shuttle_qdrant::QdrantReadyInfo
+    #[shuttle_qdrant::Qdrant(cloud_url = "{secrets.CLOUD_URL}", api_key = "{secrets.API_KEY}")]
+    qdrant: QdrantClient,
 ) -> shuttle_axum::ShuttleAxum {
-    let mut config = QdrantClientConfig::from_url(&info.url);
-
-    if let Some(api_key) = info.api_key {
-        config.set_api_key(&api_key);
-    }
-
-    let qdrant = QdrantClient::new(Some(config))?;
     let state = Arc::new(AppState { qdrant });
 
-    let router = Router::new().route("/", get(list_collections)).with_state(state);
+    let router = Router::new()
+        .route("/", get(list_collections))
+        .with_state(state);
 
     Ok(router.into())
 }
