f0b3707238c97ef06bee60ba28b98d54f0b9983b
Orhun Parmaksız <orhun@shuttle.rs>
2023-08-21 11:49:35 +0300

    chore: rename service info to metadata

--- axum/metadata/src/main.rs
+++ axum/metadata/src/main.rs
@@ -1,9 +1,9 @@
 use axum::{routing::get, Router};
-use shuttle_service_info::ServiceInfo;
+use shuttle_metadata::ServiceInfo;
 
 #[shuttle_runtime::main]
 async fn axum(
-    #[shuttle_service_info::ShuttleServiceInfo] service_info: ServiceInfo,
+    #[shuttle_metadata::ShuttleServiceInfo] service_info: ServiceInfo,
 ) -> shuttle_axum::ShuttleAxum {
     let router = Router::new().route("/", get(format!("{:?}", service_info)));
 
