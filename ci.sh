438b5e02162d0aef65c634e33e8d287a4398e9c0
Vardhan Patil <vardhanpatil@proton.me>
2023-09-02 13:20:43 +0000

    disable ci for failing crates

--- ci.sh
+++ ci.sh
@@ -8,6 +8,11 @@ DIRS=$(find . -name Cargo.toml -exec dirname {} \;)
 
 # Loop through each directory and run cargo fmt and cargo clippy, exit on failure
 for dir in $DIRS; do
+    if [ -f "$dir/.disable_ci" ]; then
+        echo "Skipping $dir"
+        continue
+    fi
+
     echo "Checking $dir"
     if [ -f "$dir/.target" ]; then
         target=$(<$dir/.target)
