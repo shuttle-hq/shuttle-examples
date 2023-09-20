2ec3f6f0e424a35b26572363511a603a649c0f54
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2023-09-20 19:41:40 +0200

    split job in half to not run out of storage

--- ci.sh
+++ ci.sh
@@ -3,8 +3,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Get all directories with Cargo.toml files
-DIRS=$(find . -name Cargo.toml -exec dirname {} \; | sort)
+# Get all directories with Cargo.toml files, alphabetically between $1 and $2
+FROM="./$1"
+TO="./$2"
+DIRS=$(find . -name Cargo.toml -exec dirname {} \; | sort | awk -v from="$FROM" -v to="$TO" '($0 >= from) && ($0 < to) {print $0}')
 
 # Loop through each directory and run cargo fmt and cargo clippy, exit on failure
 for dir in $DIRS; do
