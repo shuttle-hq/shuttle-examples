7e07f1816191693ef064fffd3397451371a362c6
Joshua Mo <102877324+joshua-mo-143@users.noreply.github.com>
2023-10-20 15:56:03 +0100

    refactor: Add examples (#93)
    * refactor: update and standardise examples
    
    * refactor: cargo fmt
    
    * refactor: correct routing for axum/postgres
    
    * refactor: readme.md
    
    * refactor: rocket static files example
    
    * refactor: clippy
    
    * feat: fix auth for SaaS template (#95)
    
    * feat: fix auth
    
    * ci: fmt
    
    * chore: v0.28.0
    
    * fix: polishing of starter templates (#97)
    
    * fix: polishing of starter templates
    
    * update init docs
    
    * add Shuttle.toml to saas
    
    * docs: update readme.md
    
    * refactor: update and standardise examples
    
    * refactor: cargo fmt
    
    * refactor: correct routing for axum/postgres
    
    * refactor: readme.md
    
    * refactor: rocket static files example
    
    * refactor: clippy
    
    * docs: update readme.md
    
    * chore: deps
    
    * chore: deps
    
    ---------
    
    Co-authored-by: oddgrd <29732646+oddgrd@users.noreply.github.com>
    Co-authored-by: jonaro00 <54029719+jonaro00@users.noreply.github.com>

---
+++ actix-web/cookie-authentication/src/main.rs
@@ -0,0 +1,77 @@
+use actix_identity::{Identity, IdentityMiddleware};
+use actix_session::{config::PersistentSession, storage::CookieSessionStore, SessionMiddleware};
+use actix_web::{
+    cookie::{time::Duration, Key},
+    error,
+    http::StatusCode,
+    middleware, web, HttpMessage as _, HttpRequest, Responder,
+};
+use actix_web::{get, web::ServiceConfig};
+use shuttle_actix_web::ShuttleActixWeb;
+
+const FIVE_MINUTES: Duration = Duration::minutes(5);
+
+#[get("/")]
+async fn index(identity: Option<Identity>) -> actix_web::Result<impl Responder> {
+    let id = match identity.map(|id| id.id()) {
+        None => "anonymous".to_owned(),
+        Some(Ok(id)) => id,
+        Some(Err(err)) => return Err(error::ErrorInternalServerError(err)),
+    };
+
+    Ok(format!("Hello {id}"))
+}
+
+#[get("/login")]
+async fn login(req: HttpRequest) -> impl Responder {
+    Identity::login(&req.extensions(), "user1".to_owned()).unwrap();
+
+    web::Redirect::to("/").using_status_code(StatusCode::FOUND)
+}
+
+#[get("/logout")]
+async fn logout(id: Identity) -> impl Responder {
+    id.logout();
+
+    web::Redirect::to("/").using_status_code(StatusCode::FOUND)
+}
+
+#[shuttle_runtime::main]
+async fn actix_web() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+    // Generate a random secret key. Note that it is important to use a unique
+    // secret key for every project. Anyone with access to the key can generate
+    // authentication cookies for any user!
+    //
+    // If the secret key is read from a file or the environment, make sure it is generated securely.
+    // For example, a secure random key (in base64 format) can be generated with the OpenSSL CLI:
+    // ```
+    // openssl rand -base64 64
+    // ```
+    //
+    // Then decoded and used converted to a Key:
+    // ```
+    // let secret_key = Key::from(base64::decode(&private_key_base64).unwrap());
+    // ```
+    let secret_key = Key::generate();
+
+    let config = move |cfg: &mut ServiceConfig| {
+        cfg.service(
+            web::scope("/")
+                .service(index)
+                .service(login)
+                .service(logout)
+                .wrap(IdentityMiddleware::default())
+                .wrap(
+                    SessionMiddleware::builder(CookieSessionStore::default(), secret_key.clone())
+                        .cookie_name("auth-example".to_owned())
+                        .cookie_secure(false)
+                        .session_lifecycle(PersistentSession::default().session_ttl(FIVE_MINUTES))
+                        .build(),
+                )
+                .wrap(middleware::NormalizePath::trim())
+                .wrap(middleware::Logger::default()),
+        );
+    };
+
+    Ok(config.into())
+}
