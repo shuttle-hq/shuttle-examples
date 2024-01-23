826fab3427cee852bdc2d6939780e9d095f4c3a6
paulotten <paulotten@users.noreply.github.com>
2024-01-23 06:21:48 -0500

    feat: qdrant resource example (#64)
    * feat: qdrant resource example
    
    * update qdrant example
    - `shuttle_qdrant::Qdrant` now returns a `QdrantClient`
    - pass in `cloud_url` and `api_key`, read from `Secrets.toml`
    
    * update versions
    
    * fix: filenames
    
    * fix: correct shuttle version
    
    * docs: add qdrant to readme
    
    ---------
    
    Co-authored-by: jonaro00 <54029719+jonaro00@users.noreply.github.com>

---
+++ axum/qdrant/src/main.rs
@@ -0,0 +1,25 @@
+use axum::{extract::State, routing::get, Router};
+use qdrant_client::prelude::*;
+use std::sync::Arc;
+
+struct AppState {
+    qdrant: QdrantClient,
+}
+
+async fn list_collections(State(state): State<Arc<AppState>>) -> String {
+    format!("{:?}\n", state.qdrant.list_collections().await)
+}
+
+#[shuttle_runtime::main]
+async fn axum(
+    #[shuttle_qdrant::Qdrant(cloud_url = "{secrets.CLOUD_URL}", api_key = "{secrets.API_KEY}")]
+    qdrant: QdrantClient,
+) -> shuttle_axum::ShuttleAxum {
+    let state = Arc::new(AppState { qdrant });
+
+    let router = Router::new()
+        .route("/", get(list_collections))
+        .with_state(state);
+
+    Ok(router.into())
+}
