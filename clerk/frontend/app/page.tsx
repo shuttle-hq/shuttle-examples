0ff54ea7281744af7a3700a2209a87fc491ebe6a
sourabpramanik <shubpramanik241@gmail.com>
2024-01-30 19:44:20 +0530

    feat: added user table and signout button

--- clerk/frontend/app/page.tsx
+++ clerk/frontend/app/page.tsx
@@ -1,16 +1,30 @@
 "use client";
-import { auth, useAuth } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
 import React from "react";
 import useSWR from "swr";
+import UsersTable from "./users-table";
+import { Loader } from "lucide-react";
+
+export interface UserSchema {
+  first_name: string;
+  last_name: string;
+  username: string;
+  email_addresses: { email_address: string; id: string }[];
+  profile_image_url: string;
+  has_image: string;
+}
 
 const getUsers = async (token: string) => {
-  const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + "/users", {
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + token,
-    },
-  });
-  return res.json();
+  const res = await (
+    await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + "/users", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+  ).json();
+
+  return res;
 };
 
 export default function Home() {
@@ -18,11 +32,16 @@ export default function Home() {
 
   const { isLoading, data, error } = useSWR("/api/users", async () => {
     const token = await getToken();
-    getUsers(token as string);
+    return getUsers(token as string);
   });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {isLoading ? "Loading" : "Data"}
+      <div className="fixed top-6 right-6">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+      {isLoading && <Loader className="w-4 h-4 animate-spin" />}
+      {!isLoading && <UsersTable users={data.data ?? []} />}
     </main>
   );
 }
