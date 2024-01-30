2dee7a539e90f29e534be898ff1a0ef95643fab2
sourabpramanik <shubpramanik241@gmail.com>
2024-01-30 19:15:06 +0530

    fix: api authentication

--- clerk/frontend/app/page.tsx
+++ clerk/frontend/app/page.tsx
@@ -1,30 +1,25 @@
 "use client";
-import { auth } from "@clerk/nextjs";
+import { auth, useAuth } from "@clerk/nextjs";
 import React from "react";
 import useSWR from "swr";
 
-const getUsers = async (sessionId: string) => {
-  const res = await (
-    await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + "/users", {
-      credentials: "include",
-      headers: {
-        "X-CLERK-SESSION_ID": sessionId,
-      },
-    })
-  ).json();
-  console.log(res);
-
-  return res;
+const getUsers = async (token: string) => {
+  const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + "/users", {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+  return res.json();
 };
 
-export default async function Home() {
-  const { sessionId } = auth();
-  // const [users, setUsers] = React.useState();
-  const { isLoading, data, error } = useSWR("/api/users", () =>
-    getUsers(sessionId as string)
-  );
-  console.log(error, data, isLoading);
+export default function Home() {
+  const { getToken } = useAuth();
 
+  const { isLoading, data, error } = useSWR("/api/users", async () => {
+    const token = await getToken();
+    getUsers(token as string);
+  });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {isLoading ? "Loading" : "Data"}
