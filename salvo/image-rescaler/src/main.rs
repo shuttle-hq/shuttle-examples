e23245d6873f743207b75598db91e6171aee3874
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-02-14 12:33:38 +0100

    feat: image rescaler

--- salvo/image-rescaler/src/main.rs
+++ salvo/image-rescaler/src/main.rs
@@ -3,17 +3,24 @@ use salvo::prelude::*;
 const IMAGE: &[u8] = include_bytes!("logo-square.png");
 
 #[handler]
-async fn get_image(res: &mut Response) {
-    let _ = res.write_body(vec![97u8]);
+async fn get_image(req: &mut Request, res: &mut Response) {
+    let width = req.param::<u32>("width").unwrap();
+    let height = req.param::<u32>("height").unwrap();
+
+    let img = image::load_from_memory_with_format(IMAGE, image::ImageFormat::Png).unwrap();
+    let img = img.resize_exact(width, height, image::imageops::FilterType::Triangle);
+    let mut buffer = std::io::BufWriter::new(std::io::Cursor::new(Vec::new()));
+    img.write_to(&mut buffer, image::ImageOutputFormat::Png)
+        .unwrap();
+    let bytes = buffer.into_inner().unwrap().into_inner();
+
+    res.add_header("content-type", "image/png", true).unwrap();
+    let _ = res.write_body(bytes);
 }
-// #[handler]
-// async fn send_file(req: &mut Request, res: &mut Response) {
-//     NamedFile::builder("/file/to/path").attached_name("image.png").send(req.headers(), res).await;
-// }
 
 #[shuttle_runtime::main]
 async fn salvo() -> shuttle_salvo::ShuttleSalvo {
-    let router = Router::new().get(get_image);
+    let router = Router::with_path("<width:num>/<height:num>").get(get_image);
 
     Ok(router.into())
 }
