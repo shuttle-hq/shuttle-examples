7385b51b299d61a1bd9b2dead016dab757ce30b9
Xuanwo <github@xuanwo.io>
2024-02-17 01:17:59 +0800

    feat: Add opendal hello world example (#142)
    * feat: Add opendal hello world example
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Move into rocket
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Update rocket/opendal-memory/README.md
    
    ---------
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ rocket/opendal-memory/src/main.rs
@@ -0,0 +1,51 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::status::BadRequest;
+use rocket::State;
+
+use opendal::Operator;
+
+struct MyState {
+    storage: Operator,
+}
+
+#[post("/<path>", data = "<data>")]
+async fn add(
+    path: String,
+    data: String,
+    state: &State<MyState>,
+) -> Result<String, BadRequest<String>> {
+    let bs = data.into_bytes();
+    let length = bs.len();
+
+    state
+        .storage
+        .write(&path, bs)
+        .await
+        .map_err(|e| BadRequest(e.to_string()))?;
+    Ok(format!("path {path} written {}B data", length))
+}
+
+#[get("/<path>")]
+async fn retrieve(path: String, state: &State<MyState>) -> Result<String, BadRequest<String>> {
+    let bs = state
+        .storage
+        .read(&path)
+        .await
+        .map_err(|e| BadRequest(e.to_string()))?;
+    let data = String::from_utf8_lossy(&bs).to_string();
+    Ok(data)
+}
+
+#[shuttle_runtime::main]
+async fn rocket(
+    #[shuttle_opendal::Opendal(scheme = "memory")] storage: Operator,
+) -> shuttle_rocket::ShuttleRocket {
+    let state = MyState { storage };
+    let rocket = rocket::build()
+        .mount("/", routes![retrieve, add])
+        .manage(state);
+
+    Ok(rocket.into())
+}
