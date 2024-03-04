49d312986cc6cf5039b2ca2d2c3a48df3b942348
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-03-04 17:01:24 +0100

    use shuttle-common model

--- _scripts/check-templates.rs
+++ _scripts/check-templates.rs
@@ -2,59 +2,16 @@
 //!
 //! ```cargo
 //! [dependencies]
-//! serde = { version = "1", features = ["derive"] }
+//! shuttle-common = "*" # always use latest version
 //! toml = "0.8"
 //! ignore = "0.4"
 //! ```
 
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Default, Serialize, Deserialize)]
-struct TemplateDefinition {
-    /// Title of the template
-    title: String,
-    /// A short description of the template
-    description: Option<String>,
-    /// Path relative to the repo root
-    path: Option<String>,
-    /// "starter" OR "template" (default) OR "tutorial"
-    #[serde(default)]
-    r#type: TemplateType,
-    /// List of areas where this template is useful. Examples: "Web app", "Discord bot", "Monitoring", "Automation", "Utility"
-    use_cases: Vec<String>,
-    /// List of keywords that describe the template. Examples: "axum", "serenity", "typescript", "saas", "fullstack", "database"
-    tags: Vec<String>,
-    /// URL to a live instance of the template (if relevant)
-    live_demo: Option<String>,
-
-    /// If this template is available in the `cargo shuttle init --template` short-hand options, add that name here
-    template: Option<String>,
-
-    /// Set this to true if this is a community template outside of the shuttle-examples repo
-    community: Option<bool>,
-    /// GitHub username of the author of the community template
-    author: Option<String>,
-    /// URL to the repo of the community template
-    repo: Option<String>,
-}
-
-#[derive(Debug, Default, Serialize, Deserialize)]
-#[serde(rename_all = "lowercase")]
-enum TemplateType {
-    Starter,
-    #[default]
-    Template,
-    Tutorial,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Schema {
-    templates: std::collections::HashMap<String, TemplateDefinition>,
-}
+use shuttle_common::templates::TemplatesSchema;
 
 fn main() {
     let s = std::fs::read_to_string("templates.toml").expect("to find file");
-    let toml: Schema = toml::from_str(&s).expect("to parse toml file");
+    let toml: TemplatesSchema = toml::from_str(&s).expect("to parse toml file");
 
     let (tx, rx) = std::sync::mpsc::channel::<String>();
 
