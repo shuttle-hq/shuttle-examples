eaf3387580b5564a4aee1b5fea314aad85eb4ebb
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2024-03-11 17:01:46 +0100

    fmt

--- bevy/hello-world/server/src/main.rs
+++ bevy/hello-world/server/src/main.rs
@@ -3,8 +3,7 @@ use tower_http::services::{ServeDir, ServeFile};
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new()
-    .nest_service(
+    let router = Router::new().nest_service(
         "/",
         ServeDir::new("dist").not_found_service(ServeFile::new("dist/index.html")),
     );
