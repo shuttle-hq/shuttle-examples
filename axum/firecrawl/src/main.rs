89a2f73e48fe0f08af18a9d121c5f58ca50bf740
Joshua Mo <joshua.mo.876@gmail.com>
2024-10-15 11:56:22 +0100

    chore: update template

--- axum/firecrawl/src/main.rs
+++ axum/firecrawl/src/main.rs
@@ -1,7 +1,10 @@
-use axum::{routing::get, Router, extract::State, Json, response::IntoResponse};
-use serde_json::json;
-
-use shuttle_firecrawl::firecrawl::FirecrawlApp;
+use axum::{extract::State, response::IntoResponse, routing::get, Json, Router};
+use firecrawl::{
+    scrape::{ScrapeFormats, ScrapeOptions},
+    FirecrawlApp,
+};
+use serde::Deserialize;
+use url::Url;
 
 #[derive(Clone)]
 struct AppState {
@@ -9,26 +12,50 @@ struct AppState {
 }
 
 impl AppState {
-    fn new(firecrawl: FirecrawlApp) -> Self {
+    fn new() -> Self {
+        let firecrawl_api_key =
+            std::env::var("FIRECRAWL_API_KEY").expect("FIRECRAWL_API_KEY to be set as an env var");
+        let firecrawl = FirecrawlApp::new(firecrawl_api_key).unwrap();
         Self { firecrawl }
     }
 }
 
-async fn scrape_page(State(state): State<AppState>) -> impl IntoResponse {
-        let scrape_response = state.firecrawl.scrape_url("https://firecrawl.dev", Some(json!({
-            "formats": ["markdown", "html"]
-        }))).await.unwrap();
+#[derive(Deserialize)]
+struct Request {
+    url: Url,
+}
 
-        Json(scrape_response)
+async fn scrape_page(
+    State(state): State<AppState>,
+    Json(json): Json<Request>,
+) -> impl IntoResponse {
+    let scrape_options = ScrapeOptions {
+        formats: Some(vec![ScrapeFormats::HTML, ScrapeFormats::Markdown]),
+        ..Default::default()
+    };
+
+    let scrape_response = state
+        .firecrawl
+        .scrape_url(&json.url, scrape_options)
+        .await
+        .unwrap();
+
+    Json(scrape_response)
 }
 
 #[shuttle_runtime::main]
-async fn main(#[shuttle_firecrawl::Firecrawl(
-    api_key = "{secrets.FIRECRAWL_API_KEY}"
-)] firecrawl: FirecrawlApp) -> shuttle_axum::ShuttleAxum {
-    let state = AppState::new(firecrawl);
+async fn main(
+    #[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore,
+) -> shuttle_axum::ShuttleAxum {
+    secrets.into_iter().for_each(|(key, val)| {
+        std::env::set_var(key, val);
+    });
 
-    let router = Router::new().route("/", get(scrape_page)).with_state(state);
+    let state = AppState::new();
+
+    let router = Router::new()
+        .route("/scrape", get(scrape_page))
+        .with_state(state);
 
     Ok(router.into())
 }
