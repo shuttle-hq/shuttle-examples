0a3a9bba7313d59e9d875d348807aff251392864
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2025-01-14 13:34:54 +0100

    chore: bump loco to 0.14.0 (#202)
    * chore: loco 0.14.0 + shuttle 0.51.0
    
    * fix: package name
    
    * update gitignore from shuttle cli

--- loco/hello-world/tests/requests/home.rs
+++ loco/hello-world/tests/requests/home.rs
@@ -1,28 +1,15 @@
 use hello_world::app::App;
-use insta::assert_debug_snapshot;
-use loco_rs::testing;
+use loco_rs::testing::prelude::*;
 use serial_test::serial;
-
-// TODO: see how to dedup / extract this to app-local test utils
-// not to framework, because that would require a runtime dep on insta
-macro_rules! configure_insta {
-    ($($expr:expr),*) => {
-        let mut settings = insta::Settings::clone_current();
-        settings.set_prepend_module_to_snapshot(false);
-        settings.set_snapshot_suffix("home_request");
-        let _guard = settings.bind_to_scope();
-    };
-}
 
 #[tokio::test]
 #[serial]
 async fn can_get_home() {
-    configure_insta!();
+    request::<App, _, _>(|request, _ctx| async move {
+        let res = request.get("/api").await;
 
-    testing::request::<App, _, _>(|request, _ctx| async move {
-        let notes = request.get("/").await;
-
-        assert_debug_snapshot!((notes.status_code(), notes.text()));
+        assert_eq!(res.status_code(), 200);
+        res.assert_json(&serde_json::json!({"app_name":"loco"}));
     })
     .await;
 }
