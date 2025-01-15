f49e826817780a136702cecddd252dc07dffb185
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2025-01-15 20:47:02 +0100

    chore: remove custom service that depends on shuttle-persist

--- custom-service/request-scheduler/request.sh
+++
@@ -1,6 +0,0 @@
-SCHEDULE="*/2 * * * * *"
-URL="http://localhost:8000/trigger-me"
-
-curl -v http://localhost:8000/crontab/set\
-  -H "Content-Type: application/x-www-form-urlencoded"\
-  -d "schedule=$SCHEDULE&url=$URL"
