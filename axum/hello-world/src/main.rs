18499f2229bda73dd16aa89faba5a8170e4559b1
oddgrd <29732646+oddgrd@users.noreply.github.com>
2025-03-28 10:42:21 +0100

    feat: emit ulid in hello world res

--- axum/hello-world/src/main.rs
+++ axum/hello-world/src/main.rs
@@ -1,12 +1,19 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, extract::State};
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn hello_world(State(state): State<AppState>,) -> String {
+    let body = format!("Hello from {}", state.id);
+    body
+}
+
+#[derive(Clone)]
+struct AppState {
+    pub id: ulid::Ulid
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let state = AppState {id: ulid::Ulid::new()};
+    let router = Router::new().route("/", get(hello_world)).with_state(state);
 
     Ok(router.into())
 }
