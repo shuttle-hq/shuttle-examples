601b33129721e3b2b00306ab0af57ef241b117c2
dcodesdev <101001810+dcodesdev@users.noreply.github.com>
2025-07-01 21:31:30 +0300

    Axum Static files updated

--- axum/static-files/src/main.rs
+++ axum/static-files/src/main.rs
@@ -1,10 +1,10 @@
-use axum::Router;
+use shuttle_axum::axum::Router;
 use tower_http::services::ServeDir;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     // ServeDir falls back to serve index.html when requesting a directory
-    let router = Router::new().nest_service("/", ServeDir::new("assets"));
+    let router = Router::new().fallback_service(ServeDir::new("assets"));
 
     Ok(router.into())
 }
