1ffca034667f047c8d7f696eb054e9ca2562a8f9
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2025-07-19 03:49:46 +0200

    chore: update loco for 0.56 (#222)

--- loco/hello-world/src/bin/shuttle.rs
+++ loco/hello-world/src/bin/shuttle.rs
@@ -2,23 +2,19 @@ use loco_rs::boot::{create_app, StartMode};
 use loco_rs::environment::Environment;
 use hello_world::app::App;
 
-use shuttle_runtime::DeploymentMetadata;
-
 #[shuttle_runtime::main]
 async fn main(
-  
-  #[shuttle_runtime::Metadata] meta: DeploymentMetadata,
+  #[shuttle_runtime::Metadata] meta: shuttle_runtime::DeploymentMetadata,
 ) -> shuttle_axum::ShuttleAxum {
-    
     let environment = match meta.env {
         shuttle_runtime::Environment::Local => Environment::Development,
         shuttle_runtime::Environment::Deployment => Environment::Production,
     };
 
-     let config = environment
+    let config = environment
         .load()
         .expect("Failed to load configuration from the environment");
-    
+
     let boot_result = create_app::<App>(StartMode::ServerOnly, &environment, config)
         .await
         .unwrap();
