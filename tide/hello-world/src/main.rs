1fd72246ce5038d31f3c29ecef0f932f72f53905
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2025-09-09 23:13:23 +0200

    chore: remove shuttle-tide

--- tide/hello-world/src/main.rs
+++
@@ -1,9 +0,0 @@
-#[shuttle_runtime::main]
-async fn tide() -> shuttle_tide::ShuttleTide<()> {
-    let mut app = tide::new();
-    app.with(tide::log::LogMiddleware::new());
-
-    app.at("/").get(|_| async { Ok("Hello, world!") });
-
-    Ok(app.into())
-}
